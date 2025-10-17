b796662a791e84b5d10a0246946cb2985e580afe
[dyad] <daniel.tokuzumi@gmail.com>
2025-10-17 17:06:22 -0300

    [dyad] Corrigindo o espaçamento inferior da seção 'Experimente Agora' em telas pequenas. - wrote 1 file(s)

--- src/components/sections/experimente-section.tsx
+++ src/components/sections/experimente-section.tsx
@@ -6,7 +6,7 @@ export const ExperimenteSection = () => {
   const agentImageSrc = "https://res.cloudinary.com/dco1sm3hy/image/upload/v1760685315/bg_aiVoice_thais2_e8mf6v.jpg";
 
   return (
-    <section className="content-section bg-black text-white relative overflow-hidden min-h-screen lg:flex-row lg:items-stretch">
+    <section className="content-section bg-black text-white relative overflow-hidden min-h-screen lg:flex-row lg:items-stretch pb-16 lg:pb-0"> {/* Adicionado pb-16 aqui */}
       
       {/* Coluna Esquerda: Conteúdo de Texto (60% em LG) */}
       <div className="section-content-wrapper relative z-10 pb-0 lg:pb-24">
@@ -23,12 +23,12 @@ export const ExperimenteSection = () => {
       </div>
 
       {/* Coluna Direita: Imagem da Agente (40% em LG) */}
-      <div className="relative w-full h-[50vh] lg:w-[40%] lg:h-auto flex-shrink-0 flex items-center justify-center"> {/* h-[30vh] alterado para h-[50vh] */}
+      <div className="relative w-full h-[50vh] lg:w-[40%] lg:h-auto flex-shrink-0 flex items-center justify-center">
         <Image
           src={agentImageSrc}
           alt="Agente de voz Thais"
           fill
-          className="object-contain brightness-110 contrast-110" // Adicionado filtro para aumentar a vivacidade
+          className="object-contain brightness-110 contrast-110"
           sizes="(max-width: 1024px) 100vw, 40vw"
           priority
         />
