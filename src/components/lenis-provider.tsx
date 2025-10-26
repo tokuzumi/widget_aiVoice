18144533ae9f1a833880ebec6b380d10e6fbf99a
[dyad] <daniel.tokuzumi@gmail.com>
2025-10-25 21:23:08 -0300

    [dyad] Removendo dependência trial do GSAP e corrigindo tipagem do Lenis - wrote 2 file(s)

--- src/components/lenis-provider.tsx
+++ src/components/lenis-provider.tsx
@@ -15,7 +15,6 @@ export const LenisProvider: React.FC<LenisProviderProps> = ({ children }) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
-      direction: "vertical",
       gestureDirection: "vertical",
       smooth: true,
       mouseMultiplier: 1,
